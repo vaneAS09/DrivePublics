f4f8625c37bcc46ee1264794d5ac28f2f3fe5c9e
Vanessa Ayala Salas <84061086+vaneAS09@users.noreply.github.com>
2022-11-18 17:56:00 -0500

    Update QuickStart.py

--- autentication/QuickStart.py
+++ autentication/QuickStart.py
@@ -1,13 +1,7 @@
 ##
 #Drives públicos
-# Autor : Vanessa Ayala Salas
+#Autor : Vanessa Ayala Salas
 #Descripción: Método encargado de generar la autenticación segura con Google drive mediante la librería pydrive
-# 
-#Cambios (Versión)
-#-------------------------------------
-#No.           Fecha            Autor                    Descripción
-# -----        ----------       --------------------     ---------------
-# 1.0          13/11/2022       Vanessa Ayala Salas      Creación de la clase.
 ##
 
 from pydrive.auth import GoogleAuth
