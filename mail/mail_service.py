121440ecf9349c53d346a2a8f596a83ec7320d8e
Vanessa Ayala Salas <84061086+vaneAS09@users.noreply.github.com>
2022-11-18 17:40:30 -0500

    Add files via upload

---
+++ mail/mail_service.py
@@ -0,0 +1,35 @@
+
+#Importar librería para envío de correos
+import smtplib 
+import json
+
+#Se importa el archivo de configuración
+
+with open('./mail/setting_mail.json') as file: setting_mail = json.load(file)
+
+
+#Se construye la clase para el servicio de envío de correo
+
+class mail_service:
+    
+    def send_mail(receiver):
+        try:
+            #se construye la estructura de envío de correo desde el archivo de configuración.
+
+            message = f"Subject: {setting_mail['MESSAGE']['subject']}\n\n{setting_mail['MESSAGE']['message']}" 
+
+            # Configuracion del servidor
+            server = smtplib.SMTP('smtp.gmail.com', 587) 
+            server.starttls() 
+            #logueo con credenciales, parametros(usuario, contraseña)
+            server.login(setting_mail['USER']['user'], setting_mail['USER']['password']) 
+            #Envío de correo, parametros(remitente, destinatario, mensaje)
+            server.sendmail(setting_mail['USER']['sender'],receiver,message) 
+            server.quit() 
+
+            print("El correo se ha enviado satisfactoriamente")
+
+        except:
+            print("Por favor valide los datos de envío de correo en el archivo"
+            + " " + "setting_mail.json"
+            )
